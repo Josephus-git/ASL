a704e455752ba44895cb6561aa0d48054935cc3d
Josephus-git <otejerejosephus@gmail.com>
2026-06-15 00:17:57 +0100

    detect

--- src/test.py
+++ src/test.py
@@ -35,7 +35,7 @@ inference_time = (time.time() - start_time) * 1000  # Convert to ms
 
 probabilities = result['pred_logits'].softmax(-1)[:,:,:-1] 
 max_probs, max_classes = probabilities.max(-1)
-keep_mask = max_probs > 0.95
+keep_mask = max_probs > 0.4
 batch_indices, query_indices = torch.where(keep_mask) 
 
 bboxes = rescale_bboxes(result['pred_boxes'][batch_indices, query_indices,:], (224,224))
